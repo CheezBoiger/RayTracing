289d89d0afe25faeba8f0644324fb4c9095c5104
Garcia <mario.garcia@intel.com>
2021-01-28 11:50:17 -0800

    clamp to 255.999 value, to fill cutoff

--- source/framebuffer/RenderTarget.cpp
+++ source/framebuffer/RenderTarget.cpp
@@ -8,9 +8,9 @@ void RenderTarget::storeColor(U32 x, U32 y, const Float3& color)
 {
     if (!surface)
         return;
-    (*surface)[(U64(width) * U64(y) + U64(x)) * 3 + 0] = U8(color.x * 255.f);
-    (*surface)[(U64(width) * U64(y) + U64(x)) * 3 + 1] = U8(color.y * 255.f);
-    (*surface)[(U64(width) * U64(y) + U64(x)) * 3 + 2] = U8(color.z * 255.f);
+    (*surface)[(U64(width) * U64(y) + U64(x)) * 3 + 0] = U8(color.x * 255.999f);
+    (*surface)[(U64(width) * U64(y) + U64(x)) * 3 + 1] = U8(color.y * 255.999f);
+    (*surface)[(U64(width) * U64(y) + U64(x)) * 3 + 2] = U8(color.z * 255.999f);
     
 }
 } // rt
